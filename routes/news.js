9a974126fcab7aca600fe02a1e893347d986bb64
Arunkumar Srisailapathi <thearunkumarsrisailapathi@gmail.com>
2022-01-22 20:17:03 +0530

    changes yet again

--- routes/news.js
+++ routes/news.js
@@ -7,7 +7,7 @@ router.get("/", async (req, res) => {
   try {
     const limit = parseInt(req.query.limit, 10) || 100;
     const start = parseInt(req.query.start, 10) || 1;
-    const news = await News.find({}).sort({ date: -1 }).skip(start - 1).limit(start + limit);
+    const news = await News.find({}).sort({ date: -1 }).skip(start - 1).limit(limit);
     res.json(news);
   } catch (err) {
     res.json({ message: err });
